0aa86bda6b93ce30a9a777b480a1755651e3cfe8
WMXPY <wm@mengw.io>
2019-11-26 10:30:59 -0600

    add route export

--- src/route/import.ts
+++ src/route/import.ts
@@ -10,6 +10,7 @@ import { QueryAccountRoute } from "./account/query";
 import { VerifyAccountRoute } from "./account/verify";
 import { InplodeOrganizationRoute } from "./organization/inplode";
 import { QueryOrganizationRoute } from "./organization/query";
+import { RegisterSubAccountRoute } from "./organization/register";
 import { VerifyOrganizationRoute } from "./organization/verify";
 import { ValidateBridgeRoute } from "./validate/bridge";
 import { ValidateDirectRoute } from "./validate/direct";
@@ -17,15 +18,16 @@ import { ValidateDirectRoute } from "./validate/direct";
 export const RouteList = [
 
     // Account
-    new QueryAccountRoute(),
     new AccountDetailRoute(),
-    new VerifyAccountRoute(),
     new LimboAccountRoute(),
+    new QueryAccountRoute(),
+    new VerifyAccountRoute(),
 
     // Organization
-    new QueryOrganizationRoute(),
-    new VerifyOrganizationRoute(),
     new InplodeOrganizationRoute(),
+    new QueryOrganizationRoute(),
+    new RegisterSubAccountRoute(),
+    new VerifyOrganizationRoute(),
 
     // Validate
     new ValidateBridgeRoute(),
