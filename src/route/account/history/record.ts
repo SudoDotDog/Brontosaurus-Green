bccb08dd58d3af8b778c9d4c217a78bf01a342bf
PCXPY <pc@wmpcxpy.com>
2019-12-02 10:31:45 -0600

    [feat] implement history record route

--- src/route/account/history/record.ts
+++ src/route/account/history/record.ts
@@ -4,13 +4,12 @@
  * @description Record
  */
 
-import { AccountActions, AccountController, IAccountModel, validateAccountAction } from "@brontosaurus/db";
+import { AccountActions, AccountController, ApplicationController, IAccountModel, IApplicationModel, validateAccountAction } from "@brontosaurus/db";
 import { ROUTE_MODE, SudooExpressHandler, SudooExpressNextFunction, SudooExpressRequest, SudooExpressResponse } from "@sudoo/express";
 import { Safe, SafeExtract } from "@sudoo/extract";
 import { HTTP_RESPONSE_CODE } from "@sudoo/magic";
 import { createGreenAuthHandler } from "../../../handlers/handlers";
 import { autoHook } from "../../../handlers/hook";
-import { createRandomTempPassword } from "../../../util/auth";
 import { ERROR_CODE, panic } from "../../../util/error";
 import { BrontosaurusRoute } from "../../basic";
 
@@ -21,7 +20,7 @@ export type AccountHistoryRecordRouteBody = {
     readonly application: string;
     readonly by: string;
 
-    readonly content?: string;
+    readonly content: string;
 };
 
 export class AccountHistoryRecordRoute extends BrontosaurusRoute {
@@ -46,6 +45,14 @@ export class AccountHistoryRecordRoute extends BrontosaurusRoute {
 
             const target: string = body.directEnsure('target');
             const type: keyof AccountActions = body.directEnsure('type');
+            const application: string = body.directEnsure('application');
+            const content: string = body.directEnsure('content');
+
+            const by: string = body.directEnsure('by');
+
+            if (!validateAccountAction(type)) {
+                throw panic.code(ERROR_CODE.INVALID_ACCOUNT_ACTION, type);
+            }
 
             const account: IAccountModel | null = await AccountController.getAccountByUsername(target);
 
@@ -53,21 +60,23 @@ export class AccountHistoryRecordRoute extends BrontosaurusRoute {
                 throw this._error(ERROR_CODE.ACCOUNT_NOT_FOUND, target);
             }
 
-            if (!validateAccountAction(type)) {
-                throw panic.code(ERROR_CODE.INVALID_ACCOUNT_ACTION, type);
+            const self: IAccountModel | null = await AccountController.getAccountByUsername(by);
+
+            if (!self) {
+                throw this._error(ERROR_CODE.ACCOUNT_NOT_FOUND, by);
             }
 
-            account.pushHistory()
+            const actionApplication: IApplicationModel | null = await ApplicationController.getApplicationByKey(application);
 
-            const tempPassword: string = createRandomTempPassword();
-            account.limbo = true;
-            account.setPassword(tempPassword);
-            account.resetAttempt();
+            if (!actionApplication) {
+                throw this._error(ERROR_CODE.APPLICATION_NOT_FOUND, application);
+            }
+
+            account.pushHistory(type, account._id, self._id, content, undefined);
 
             await account.save();
 
-            res.agent.add('limbo', Boolean(account.limbo));
-            res.agent.add('tempPassword', tempPassword);
+            res.agent.add('account', account.id);
         } catch (err) {
 
             res.agent.fail(HTTP_RESPONSE_CODE.BAD_REQUEST, err);
