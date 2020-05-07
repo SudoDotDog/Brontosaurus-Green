a9f7fad332397276391f86e1c5f826b532f9b9b9
WMXPY <wm@mengw.io>
2020-05-07 12:48:47 -0500

    upgrade express, add error

--- src/util/error.ts
+++ src/util/error.ts
@@ -99,7 +99,7 @@ export const ERROR_LIST: Record<ERROR_CODE, string> = {
 
     [ERROR_CODE.INVALID_ACCOUNT_ACTION]: 'Invalid account action type: "{}"',
 
-    [ERROR_CODE.REQUEST_DOES_MATCH_PATTERN]: 'Request does not match pattern',
+    [ERROR_CODE.REQUEST_DOES_MATCH_PATTERN]: 'Request does not match pattern: "{}"',
     [ERROR_CODE.REQUEST_FORMAT_ERROR]: 'Request format error: "{}", should be: "{}", but: "{}"',
 
     [ERROR_CODE.APPLICATION_NOT_FOUND]: 'Application: "{}" not found',
