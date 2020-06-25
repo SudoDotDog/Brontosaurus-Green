a039d24726a54b49956bc954ede84aa45636cd6e
WMXPY <wm@mengw.io>
2020-06-25 10:28:39 -0500

    add pattern to more routes

--- src/route/organization/query.ts
+++ src/route/organization/query.ts
@@ -5,14 +5,20 @@
  */
 
 import { IOrganizationModel, OrganizationController, TagController } from "@brontosaurus/db";
-import { ROUTE_MODE, SudooExpressHandler, SudooExpressNextFunction, SudooExpressRequest, SudooExpressResponse } from "@sudoo/express";
-import { Safe, SafeExtract } from "@sudoo/extract";
+import { createStringedBodyVerifyHandler, ROUTE_MODE, SudooExpressHandler, SudooExpressNextFunction, SudooExpressRequest, SudooExpressResponse } from "@sudoo/express";
 import { HTTP_RESPONSE_CODE } from "@sudoo/magic";
+import { createListPattern, createStrictMapPattern, createStringPattern, Pattern } from "@sudoo/pattern";
+import { fillStringedResult, StringedResult } from "@sudoo/verify";
 import { ObjectID } from "bson";
 import { createGreenAuthHandler } from "../../handlers/handlers";
 import { autoHook } from "../../handlers/hook";
 import { ERROR_CODE, panic } from "../../util/error";
 import { BrontosaurusRoute } from "../basic";
+
+const bodyPattern: Pattern = createStrictMapPattern({
+
+    tags: createListPattern(createStringPattern()),
+});
 
 export type QueryOrganizationRouteBody = {
 
@@ -26,12 +32,13 @@ export class QueryOrganizationRoute extends BrontosaurusRoute {
 
     public readonly groups: SudooExpressHandler[] = [
         autoHook.wrap(createGreenAuthHandler(), 'Green'),
+        autoHook.wrap(createStringedBodyVerifyHandler(bodyPattern), 'Body Verify'),
         autoHook.wrap(this._queryOrganizationRoute.bind(this), 'Query Organization'),
     ];
 
     private async _queryOrganizationRoute(req: SudooExpressRequest, res: SudooExpressResponse, next: SudooExpressNextFunction): Promise<void> {
 
-        const body: SafeExtract<QueryOrganizationRouteBody> = Safe.extract(req.body as QueryOrganizationRouteBody, this._error(ERROR_CODE.INSUFFICIENT_INFORMATION));
+        const body: QueryOrganizationRouteBody = req.body;
 
         try {
 
@@ -41,12 +48,17 @@ export class QueryOrganizationRoute extends BrontosaurusRoute {
 
             let query: Record<string, any> = {};
 
-            const tagNames: string[] = body.direct('tags');
-            if (!Array.isArray(tagNames)) {
+            const verify: StringedResult = fillStringedResult(req.stringedBodyVerify);
+
+            if (!verify.succeed) {
+                throw panic.code(ERROR_CODE.REQUEST_DOES_MATCH_PATTERN, verify.invalids[0]);
+            }
+
+            if (!Array.isArray(body.tags)) {
                 throw panic.code(ERROR_CODE.INSUFFICIENT_SPECIFIC_INFORMATION, 'tags');
             }
 
-            query = await this._attachTag(tagNames, query);
+            query = await this._attachTag(body.tags, query);
 
             const organizations: IOrganizationModel[] = await OrganizationController.getOrganizationsByQuery(query);
 
